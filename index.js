5f0bf26deb34f8fc8fb12f9197599d18bb02e7d1
Shezan-uxd <rakibulshezan97@gmail.com>
2022-01-19 12:50:28 +0600

    Crud on File System

--- index.js
+++ index.js
@@ -2,15 +2,47 @@
 const http = require("http");
 
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
+const data = require("./lib/data");
 
 //app object -module scaffholding
 
 const app = {};
 
-//configuration
-app.config = { port: 3000 };
+//testing file system
+// @TODO: pore muche dibo
 
-//create server
+//config
+app.config = {
+  port: 3000,
+};
+
+//data file create
+data.create(
+  "test",
+  "newFile",
+  { name: "Bangladesh", language: "bangla" },
+  function (err) {
+    console.log(`error was ${err}`);
+  }
+);
+
+//data file read
+data.read("test", "newFile", function (err, result) {
+  console.log(err, result);
+});
+
+//data file update
+data.update(
+  "test",
+  "newFile",
+  { name: "England", language: "English" },
+  function (err, result) {
+    console.log(err, result);
+  }
+);
+
+//data file delete
 
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
