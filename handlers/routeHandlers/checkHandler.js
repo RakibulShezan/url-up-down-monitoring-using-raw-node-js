4be10c22e860529d32b5c9a25029c69522c9acdc
Shezan-uxd <rakibulshezan97@gmail.com>
2022-01-20 16:38:54 +0600

    Check Url Api

---
+++ handlers/routeHandlers/checkHandler.js
@@ -0,0 +1,59 @@
+const data = require("../../lib/data");
+const { hash } = require("../../helpers/utilities");
+const { parseJSON } = require("../../helpers/utilities");
+const tokenHandler = require("./tokenHandler");
+
+const handler = {};
+
+handler.checkHandler = (requestProperties, callBack) => {
+  const acceptedMethods = ["get", "post", "put", "delete"];
+
+  if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+    handler._check[requestProperties.method](requestProperties, callBack);
+  } else {
+    callBack(405);
+  }
+};
+
+handler._check = {};
+
+handler._check.post = (requestProperties, callback) => {
+  //validate inputs
+  let protocol =
+    typeof requestProperties.body.protocol === "string" &&
+    ["http", "https"].indexOf(requestProperties.body.protocol) > -1
+      ? requestProperties.body.protocol
+      : false;
+
+  let url =
+    typeof requestProperties.body.url === "string" &&
+    requestProperties.body.url.trim().length > 0
+      ? requestProperties.body.url
+      : false;
+
+  let method =
+    typeof requestProperties.body.method === "string" &&
+    ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1
+      ? requestProperties.body.method
+      : false;
+  let successCodes =
+    typeof requestProperties.body.successCodes === "object" &&
+    requestProperties.body.successCodes instanceof Array
+      ? requestProperties.body.successCodes
+      : false;
+
+  let timeoutSeconds =
+    typeof requestProperties.body.timeoutSeconds === "number" &&
+    requestProperties.body.timeoutSeconds % 1 === 0 &&
+    requestProperties.body.timeoutSeconds >= 1 &&
+    requestProperties.body.timeoutSeconds <= 5
+      ? requestProperties.body.timeoutSeconds
+      : false;
+};
+
+handler._check.get = (requestProperties, callback) => {};
+handler._check.put = (requestProperties, callback) => {};
+
+handler._check.delete = (requestProperties, callback) => {};
+
+module.exports = handler;
