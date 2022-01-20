81b4e7e032a02f4651efaacfde21be283117dcdf
Shezan-uxd <rakibulshezan97@gmail.com>
2022-01-20 14:49:20 +0600

    Authentication with token

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -29,4 +29,21 @@ utilities.hash = (string) => {
   }
 };
 
+// Create random string(Token)
+utilities.createRandomString = (strLength) => {
+  let length = strLength;
+  length = typeof strLength === "number" && strLength > 0 ? strLength : false;
+  if (length) {
+    let possibleCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i <= length; i += 1) {
+      let randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  } else return false;
+};
+
 module.exports = utilities;
